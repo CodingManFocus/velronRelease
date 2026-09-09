d47deead7e916ffd5f64a2d364631f9f9deeb2ba
CodingManFocus <77488524+CodingManFocus@users.noreply.github.com>
2026-09-09 22:47:27 +0900

    Move Installer development to velronInstaller

--- installer/linux/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-launcher_dir=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd)
-case "$(uname -m)" in
-  x86_64|amd64) architecture=x64 ;;
-  aarch64|arm64) architecture=arm64 ;;
-  *) printf '%s\n' 'Velron Installer supports x64 and ARM64.' >&2; exit 1 ;;
-esac
-exec "$launcher_dir/$architecture/velron-installer" "$@"
