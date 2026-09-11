94be85214d12b5d74e5f5640900706ca0ab0af24
CodingManFocus <77488524+CodingManFocus@users.noreply.github.com>
2026-09-11 18:46:37 +0900

    Follow installer main in compatibility scripts and install documentation

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Compatibility entry point. Installer development lives in velronInstaller.
 set -eu
 
-installer_url='https://raw.githubusercontent.com/CodingManFocus/velronInstaller/4f2a7538c238805aa5e62853baa98a29d144625a/install.sh'
+installer_url='https://raw.githubusercontent.com/CodingManFocus/velronInstaller/main/install.sh'
 installer_path=$(mktemp "${TMPDIR:-/tmp}/velron-install.XXXXXXXX")
 trap 'rm -f "$installer_path"' EXIT
 trap 'exit 1' HUP INT TERM
